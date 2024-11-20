459c8c8b5b5a7321ccc945f88143b3851b4927b2
tliataylor8 <140763571+tliataylor8@users.noreply.github.com>
2024-11-20 15:39:37 -0500

    Update

---
+++ src/Girl.java
@@ -0,0 +1,15 @@
+
+public class Girl extends Person{
+	static double   ageFactor = 1.1;
+	   
+	   public Girl(String string) {
+		// TODO Auto-generated constructor stub
+	}
+	public String talk() {
+	       return(super.talk() + " ... but I love Java class.");
+	   }
+	   public String walk() {
+	       return("I am now walking");
+	   }
+
+}
